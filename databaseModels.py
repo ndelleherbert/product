d35c0cba3ad5d640fa1a8cdd260b4958923106b7
Ndelle Herbert <ndelleh04@gmail.com>
2025-12-06 12:34:21 +0100

    Create Product model with SQLAlchemy ORM and define products table schema

--- databaseModels.py
+++ databaseModels.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Float,Integer,String
+from sqlalchemy import Column, Float, Integer, String
 from database import Base
 
 class Product(Base):
-	id = Column(Integer, primary_key=True, index=True)
-	name = Column(String, index=True)
-	description = Column(String, index=True)
-	price = Column(Float, index=True)
-	quantit = Column(Integer, index = True)
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Float, index=True)
+    quantity = Column(Integer, index=True)   # FIXED spelling
